a08efaa0ddc993c4eb46674c1d35e0086e745649
Temi Bankole <temibankole5@gmail.com>
2025-12-11 22:55:35 -0700

    remove alert import

--- src/screens/NewPomodoroScreen.jsx
+++ src/screens/NewPomodoroScreen.jsx
@@ -4,7 +4,6 @@ import {
   ImageBackground,
   Image,
   Pressable,
-  Alert
 } from 'react-native';
 import { useState, useEffect, useRef, useCallback } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
