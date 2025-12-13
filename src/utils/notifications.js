16db52d7ffe6625b0b9f8be7af233d20aaeaa727
Temi Bankole <temibankole5@gmail.com>
2025-12-13 15:52:13 -0700

    making a different notifcation for when the break ends

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -2,12 +2,24 @@ import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { stopAlarm } from './soundPlayer';
 
-export async function notifyTimesUp() {
+export async function notifyTimesUp(sessionType) {
   const enabled = await AsyncStorage.getItem('notifications');
   if (enabled === 'true') {
-    Alert.alert('Now, you can start your break!', '', 
-      [ {text: "OK", onPress: () => stopAlarm()}], //stop alarm when alart is closed
-      {cancelable : false }
+    let message = '';
+
+    if (sessionType === 'work') {
+      message = 'Now, you can start your break!';
+    } else if (sessionType === 'break') {
+      message = 'Continue working hard 💪🏾!';
+    } else {
+      message = 'Session finished!';
+    }
+
+    Alert.alert(
+      message,
+      '',
+      [{ text: "OK", onPress: () => stopAlarm() }], // stop alarm when alert is closed
+      { cancelable: false }
     );
   }
 }
