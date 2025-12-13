f3573e33f98dd15088a13ec63290380e227b43ac
Temi Bankole <temibankole5@gmail.com>
2025-12-13 15:18:53 -0700

    added functionality so that the timer stops playing after the alert notification is closed

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -1,9 +1,13 @@
 import { Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { stopAlarm } from './soundPlayer';
 
 export async function notifyTimesUp() {
   const enabled = await AsyncStorage.getItem('notifications');
   if (enabled === 'true') {
-    Alert.alert('Times Up!', 'Your timer has finished.');
+    Alert.alert('Now, you can start your break!', '', 
+      [ {text: "OK", onPress: () => stopAlarm()}], //stop alarm when alart is closed
+      {cancelable : false }
+    );
   }
 }
